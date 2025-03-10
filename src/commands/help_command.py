187975bdeff537b8cfeca67e5eaf310052202136
nahtanpng <nathan.ferreiira13@gmail.com>
2025-03-10 18:40:53 -0300

    style: add thumbnail on help embed

--- src/commands/help_command.py
+++ src/commands/help_command.py
@@ -8,6 +8,10 @@ class CustomHelpCommand(commands.HelpCommand):
 
     async def send_bot_help(self, mapping):
         help_embed = discord.Embed(title="Cheapo Commands", color=discord.Color.yellow())
+
+        image = discord.File("src/assets/bot_icon.png", "bot_icon.png")
+        help_embed.set_thumbnail(url="attachment://bot_icon.png")
+
         help_embed.description = """
 💰 **Economy Commands**:
 - `c!balance` - Check your coin balance.
@@ -16,4 +20,4 @@ class CustomHelpCommand(commands.HelpCommand):
 
         # Send the embed
         channel = self.get_destination()
-        await channel.send(embed=help_embed)
+        await channel.send(embed=help_embed, file=image)
